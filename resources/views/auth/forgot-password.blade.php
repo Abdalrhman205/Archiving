ae2177fe0d96816d38edbfe03b3f8a9dbdee0a0d
Abdo-Bzmazem <147770377+Abdalrhman205@users.noreply.github.com>
2024-08-14 14:46:09 +0200

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,43 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!DOCTYPE html>
+<html lang="en">
+@include('layout.head')
+
+<body>
+    <div id="preloader">
+        <div class="loader"></div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <!-- preloader area end -->
+    <!-- password reset area start -->
+    <div class="password-reset-area  login-s2 ">
+        <div class="container">
+            <div class="password-reset-box login-box ptb--100 h-100vh d-flex justify-content-center align-items-center">
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="login-form-head text-center">
+                        <h4 class="">إعادة تعيين كلمة المرور</h4>
+                        <p class="">نسيت كلمة المرور؟ لا مشكلة. فقط أخبرنا بعنوان بريدك الإلكتروني وسوف نرسل لك رابط إعادة تعيين كلمة المرور.</p>
+                    </div>
+                    <!-- Email Address -->
+                    <div class="login-form-body">
+                        <div class="form-gp">
+                            <input placeholder="Email" name="email" :value="old('email')" required autofocus type="email" id="email">
+                            <i class="ti-email"></i>
+                            <div class="text-danger">
+                                @error('email')
+                                    {{ $message }}
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="submit-btn-area">
+                            <button id="form_submit" type="submit">{{ __('Email Password Reset Link') }}<i class="ti-arrow-right"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
+    </div>
+    <!-- password reset area end -->
+    @include('layout.scripts')
+</body>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
